ddaafc4f2718ab768fa4eb4b984a39abfba8bc6c
ming <346663153@qq.com>
2016-12-22 20:10:16 +0800

    a

--- js/service-details.js
+++ js/service-details.js
@@ -2,6 +2,24 @@
  * Created by Administrator on 2016/12/12.
  */
 $(document).ready(function () {
+
+    var start;
+    var end;
+    var duration = 0;
+    start = new Date();
+    $(window).bind('beforeunload', function(e) {
+        end = new Date();//用户退出时间
+        duration = end.getTime() - start.getTime();
+        duration = duration/1000;//取的是秒
+        $.ajax({
+            type: 'POST',
+            async: false,
+            url: 'url',
+            data: {a:1,b:duration}
+        });
+    });
+
+
     $("#previewTab li").click(function () {
         $("#previewTab").children("li").removeClass("active");
         $(this).addClass("active");
